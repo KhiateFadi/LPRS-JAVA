6154e0ba729574c9011cb9a997b01bfcb00ae5c7
KhiateFadi <56584146+KhiateFadi@users.noreply.github.com>
2021-01-18 15:59:28 +0100

    page d'accueil

--- LPRS-JAVA/src/View/Accueil.java
+++ LPRS-JAVA/src/View/Accueil.java
@@ -3,6 +3,11 @@ package View;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Accueil {
 
@@ -38,6 +43,32 @@ public class Accueil {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Bienvenue");
+		lblNewLabel.setBounds(177, 48, 125, 23);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Connexion");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				connexion connexion = new connexion();
+				connexion.frame.setVisible(true);
+				
+			}
+		});
+		btnNewButton.setBounds(177, 99, 125, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Inscription");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				inscription inscription = new inscription();
+				inscription.frame.setVisible(true);
+				
+			}
+		});
+		btnNewButton_1.setBounds(177, 173, 125, 23);
+		frame.getContentPane().add(btnNewButton_1);
 	}
-
 }
---
+++ LPRS-JAVA/src/View/inscription.java
@@ -0,0 +1,40 @@
+package View;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class inscription {
+
+	JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+
+			public void run() {
+				try {
+					inscription window = new inscription();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		
+	/**
+	 * Create the application.
+	 */
+	public inscription() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
