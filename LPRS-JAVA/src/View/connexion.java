99e26a951427c8128378282c939782b542510ae9
KhiateFadi <56584146+KhiateFadi@users.noreply.github.com>
2021-01-18 16:36:40 +0100

    affichage page connexion + inscription

--- LPRS-JAVA/src/View/connexion.java
+++ LPRS-JAVA/src/View/connexion.java
@@ -3,10 +3,16 @@ package View;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class connexion {
 
 	JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -37,6 +43,32 @@ public class connexion {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Connectez vous");
+		lblNewLabel.setBounds(154, 36, 90, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(154, 86, 96, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(154, 140, 96, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Valider");
+		btnNewButton.setBounds(154, 199, 96, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JLabel lblNewLabel_1 = new JLabel("Email");
+		lblNewLabel_1.setBounds(54, 89, 49, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Mots de passe");
+		lblNewLabel_2.setBounds(54, 143, 71, 14);
+		frame.getContentPane().add(lblNewLabel_2);
 	}
-
 }
--- LPRS-JAVA/src/View/inscription.java
+++ LPRS-JAVA/src/View/inscription.java
@@ -3,10 +3,18 @@ package View;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class inscription {
 
 	JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -35,6 +43,51 @@ public class inscription {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Inscription");
+		lblNewLabel.setBounds(163, 25, 75, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nom");
+		lblNewLabel_1.setBounds(46, 53, 49, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(156, 50, 96, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Pr\u00E9nom");
+		lblNewLabel_2.setBounds(46, 96, 49, 14);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Email");
+		lblNewLabel_3.setBounds(46, 138, 49, 14);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Mots de passe");
+		lblNewLabel_4.setBounds(46, 183, 89, 14);
+		frame.getContentPane().add(lblNewLabel_4);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(156, 93, 96, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(156, 135, 96, 20);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(156, 180, 96, 20);
+		frame.getContentPane().add(textField_3);
+		textField_3.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Valider");
+		btnNewButton.setBounds(152, 215, 100, 23);
+		frame.getContentPane().add(btnNewButton);
 	}
 
 }
