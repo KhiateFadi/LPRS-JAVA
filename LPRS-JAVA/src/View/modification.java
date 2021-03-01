300e23ba3cd29e95217b8db45d11e4bfa4a350a1
iliassalmi <56584061+iliassalmi@users.noreply.github.com>
2021-03-01 15:04:29 +0100

    test1

--- LPRS-JAVA/src/View/modification.java
+++ LPRS-JAVA/src/View/modification.java
@@ -1,10 +1,7 @@
 package View;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JTextField;
-import java.awt.BorderLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
